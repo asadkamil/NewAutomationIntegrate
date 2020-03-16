b0f3c538a3ab63ab9ede7736a25c8c1b948a7040
Er. Asad Kamil <60423270+asadkamil@users.noreply.github.com>
2020-03-16 16:46:47 +0530

    ss

--- src/POM/LoginPage.java
+++ src/POM/LoginPage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 public class LoginPage{
-
+	
 	//declare the elements
 	@FindBy(xpath="//input[@placeholder='Email']")
 	private WebElement unTB;
